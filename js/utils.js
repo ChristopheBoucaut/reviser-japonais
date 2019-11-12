df3800f3315964281fb44fdffb4bd7c50277f7b3
Christophe BOUCAUT <christophe.boucaut@geneanet.org>
2019-11-12 14:05:02 +0100

    Ajout de méthode utils.

--- js/utils.js
+++ js/utils.js
@@ -8,16 +8,16 @@ Revision.utils = (function () {
          * Permet de mélanger un array de façon random.
          * Source de la méthode : https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
          *
-         * @param {Array} arr [description]
+         * @param {Array} arr
          *
-         * @return {Array} [description]
+         * @return {Array}
          */
         shuffle: function (arr) {
 
             var currentIndex = arr.length, temporaryValue, randomIndex;
 
             while (0 !== currentIndex) {
-                randomIndex = Math.floor(Math.random() * currentIndex);
+                randomIndex = Revision.utils.random(currentIndex);
                 currentIndex -= 1;
 
                 temporaryValue = arr[currentIndex];
@@ -27,5 +27,35 @@ Revision.utils = (function () {
 
             return arr;
         },
+
+        /**
+         * Retourne un nom random entre 0 et max (max non compris).
+         *
+         * @param {Integer} max
+         *
+         * @return {Integer}
+         */
+        random: function (max) {
+            return Math.floor(Math.random() * max);
+        },
+
+        /**
+         * Génère un array avec tous le vocabulaire.
+         *
+         * @param {Object} wordsObj
+         *
+         * @return {Array}
+         */
+        wordsToArray: function (wordsObj) {
+            var words = [];
+
+            for (var groupName in wordsObj) {
+                for (var i = 0; i < wordsObj[groupName].length; i++) {
+                    words.push(wordsObj[groupName][i]);
+                }
+            }
+
+            return words;
+        }
     };
 })();
