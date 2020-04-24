357807ee993f59dd7bc6aeea932d325190bca6a8
Christophe BOUCAUT <boucaut.christophe@gmail.org>
2020-04-24 12:39:59 +0200

    Mise à jour de la page de révision des particules.

--- js/data/grammaire.js
+++ js/data/grammaire.js
@@ -3,147 +3,165 @@ Revision.grammaire = (function () {
 
     return {
         "particules": {
-            "の": {
-                "exprimer la possession": [
-                    "possesseurのpossédé",
-                    "リンクの馬 : le cheval de Link"
-                ],
-                "lier un objet/lieu avec un repère spatial": [
-                    "ベッドの上 : Sur le lit",
-                    "ベッドの後ろ : Derrière le lit"
-                ]
-            },
-            "と": {
-                "exprimer l’énumération": [
-                    "Il se place entre 2 noms (mais pas entre 2 adjectifs ou 2 verbes).",
-                    "マリオの犬と猫と魚 : le chien, le chat et le poisson"
-                ],
-                "exprimer la notion de “avec quelqu'un” si utilisée seule": [
-                    "マリオと : avec Mario"
-                ]
-            },
-            "や": {
-                "exprimer l’énumération non exhaustive": [
-                    "Cette particule s’utilise comme と mais sous entend que la liste n’est pas complète.",
-                    "マリオやルイージ : Mario et Luigi, etc."
-                ]
-            },
-            "は": {
-                "exprimer le thème de la phrase": [
-                    "On va la prononcer comme わ sous sa forme particule.",
-                    "Il s’agit d’un nom ou groupe nominal, toujours placé en début de phrase et qui indique le sujet de la phrase.",
-                    "Dans le cas où plusieurs phrases s’enchaînent avec le même thème, ce dernier n’a pas besoin d’être répété.",
-                    "Si le thème est sous-entendu par le contexte, il n’est pas nécessaire de le mentionner.",
-                    "リンクは男だ : Au sujet de Link, c’est un homme.",
-                    "私は男だ。: Je suis un homme.",
-                    "Le thème peut correspondre au sujet du verbe : 私は",
-                    "Le thème peut correspondre un lieu où il se passe quelque chose : 日本では",
-                    "Le thème peut correspondre un lieu où il ne se passe rien : 日本には",
-                    "Le thème peut correspondre une direction : 日本へは",
-                    "Le thème peut correspondre un complément de temps : 今日は",
-                    "Le thème peut correspondre un point de départ : ２時からは",
-                    "Le thème peut correspondre un point d’arrivé : フランスまでは",
-                    "Le thème peut correspondre un COD (pas de particule を ici) : ミルクは"
-                ]
-            },
-            "に": {
-                "Désigner un lieu où il ne se passe rien": [
-                    "Il va de pair avec un verbe de présence (いる / ある)",
-                    "私はホテルにいる。: Je suis à l’hôtel.",
-                    "本は空港にある。: Le livre est à l’aéroport."
-                ],
-                "Désigner le point d’arrivé lors d’un mouvement": [
-                    "ナルトはレストランに行く。: Naruto va au restaurant."
-                ],
-                "Désigner le COI": [
-                    "vis à vis de quoi est faite l’action, à qui ?, pour qui ?, contre qui ?",
-                    "ブルマはヤマチャにカプセルを売る。: Bulma vend des capsules à Yamada."
-                ],
-                "Désigner le changement": [
-                    "ナメック星は火山に変わる。: La planète Namek se change en volcan."
-                ],
-                "Désigner la personne que l’on rencontre": [
-                    "ピっコロはセルに会う。: Picolo rencontre Cell."
-                ],
-                "Désigner le complément de temps quand il s’agit d’un point ou d’une période précise": [
-                    "6時に : à 6h",
-                    "19世紀に : au XIXème siècle"
-                ]
-            },
-            "で": {
-                "Exprimer les lieux où il se passe quelque chose": [
-                    "ナルトはレストランで食べる。: Naruto mange au restaurant.",
-                    "ひなたは銀行でサスケとテンテンと話す。: Hinata parle avec Sasuke et Tenten à la banque.",
-                    "イタチとサスケは神社の前で飲む。: Itachi et Sasuke boivent devant le temple."
-                ],
-                "Le moyen utilisé pour effectuer une action.": [
-                    "私はタクシーで行く。: Je vais en taxi.",
-                    "サクラはバスで来る。: Sakura vient en bus.",
-                    "ナルトはスプーンで食べる。: Naruto mange avec une cuillère."
-                ]
-            },
-            "へ": {
-                "Indiquer la direction avec un verbe de mouvement directionnel.": [
-                    "On va la prononcer comme え sous sa forme particule.",
-                    "ナルトはレストランへ行く。: Naruto va en direction du restaurant.",
-                    "トランクスはバイクで西の都へ行く。: Trunks va en direction de la capitale de l’ouest en moto.",
-                    "A noter qu’en japonais, on ne se dirige pas vers quelqu’un mais vers le lieu de cette personne.",
-                    "クリリンはナルトの所へ向かう。: Krilin se dirige vers Naruto."
-                ]
-            },
-            "を": {
-                "indiquer le COD": [
-                    "qui subit l’action du verbe",
-                    "On va la prononcer comme お sous sa forme particule.",
-                    "私はミルクを飲む。: Je bois du lait.",
-                    "Le COD, comme en français, se trouve toujours au plus proche du verbe."
-                ],
-                "Exprimer un lieu dans une idée de parcours (avec un verbe de mouvement)": [
-                    "鳥は空を飛ぶ。: L’oiseau vole dans le ciel.",
-                    "私は公園を歩く。: Je marche dans le parc.",
-                    "魚は海を泳ぐ。: Le poisson nage dans la mer."
-                ]
-            },
-            "から": {
-                "Exprimer le point de départ": [
-                    "avec une idée de limite / jusqu’à",
-                    "から est placée devant まで si les 2 sont préentes",
-                    "Lは電車で大阪から広島まで行く。: L va d’Osaka à Hiroshima en train.",
-                    "Lの授業は１０時半までだ。: Le cours de L termine à 10h30.",
-                    "Lは心から祈る。: L prie de tout son coeur."
-                ]
-            },
-            "まで": {
-                "Exprimer le point d’arrivé": [
-                    "avec une idée de limite / jusqu’à",
-                    "から est placée devant まで si les 2 sont préentes",
-                    "Lは電車で大阪から広島まで行く。: L va d’Osaka à Hiroshima en train.",
-                    "Lの授業は１０時半までだ。: Le cours de L termine à 10h30.",
-                    "Lは心から祈る。: L prie de tout son coeur."
-                ]
-            },
-            "が": {
-                "Exprimer le sujet du verbe": [
-                    "On l’utilise lorsque le thème est différent du sujet du verbe. (日本では熊が猿を食べる。: Au Japon, les ours mangent des singes.)",
-                    "Dans une phrase sans thème, pour forcer sur le sujet du verbe ou donner une nouvelle information. (サンがもののけ姫だ。: C’est San qui est la princesse Mononoké. / イノシシが来る！: Les sangliers arrivent !)"
-                ],
-                "Exprimer une présence": [
-                    "木の前にはライオンがいる。: Devant l’arbre, il y a un lion.",
-                    "ライオンがいる。: Il y a un lion."
-                ],
-                "Exprimer le verbe avoir": [
-                    "Il n’y a pas de verbe dédié.",
-                    "キリンは角がある。: La girafe a des cornes.",
-                    "私は妹がいる。: J’ai une petite soeur."
-                ]
-            },
-            "も": {
-                "introduire dans une phrase, une chose identique à une autre précédemment énoncée": [
-                    "équivaut au “aussi”, “de même”, etc.",
-                    "レモンは果物だ。: Le citron est un fruit.",
-                    "レモンも果物だ。: Le citron aussi est un fruit."
-                ],
-            }
+            "の": [
+                "Particule de liaison : Elle permet d'exprimer la possession.",
+                "Particule de liaison : Elle permet de lier un objet/lieu avec un repère spatial.",
+                "Particule syntaxique : Elle permet d'exprimer la notion de “avec” si utilisée seule lorsqu'il s'agit d'un être vivant.",
+            ],
+            "と": [
+                "Particule de liaison : Elle permet d'exprimer l'énumération (équivalent du et en français). Il se place entre 2 noms (mais pas entre 2 adjectifs ou 2 verbes).",
+            ],
+            "との": [
+                "Particule de liaison : Elle permet d'exprimer l'énumération quelqu'un et moi (avec).",
+            ],
+            "とは": [
+                "Particule de liaison : Elle permet de donner une définition.",
+            ],
+            "や": [
+                "Particule de liaison : Elle permet d'exprimer l'énumération non exhaustive. Il se place entre 2 noms (mais pas entre 2 adjectifs ou 2 verbes).",
+            ],
+            "やら": [
+                "Particule de liaison : Elle permet d'exprimer l'énumération non exhaustive et se place après chaque éléments.",
+            ],
+            "だの": [
+                "Particule de liaison : Elle permet d'exprimer l'énumération non exhaustive et se place après chaque éléments.",
+            ],
+            "か": [
+                "Particule de liaison : Elle permet de dresser un choix dans une liste.",
+                "Particule de fin de phrases : Elle permet de poser une question \"totale\" (réponse oui/non).",
+            ],
+            "なり": [
+                "Particule de liaison : Elle permet de dresser un choix non exhaustif dans une liste.",
+            ],
+            "に": [
+                "Particule de liaison : Elle permet de dresser des énumérations par paire ou bien dans l'idée d'ajout successif.",
+                "Particule syntaxique : Elle permet de désigner un lieu où il ne se passe rien. Il va de pair avec un verbe de présence.",
+                "Particule syntaxique : Elle permet de désigner le point d'arrivé lors d'un mouvement.",
+                "Particule syntaxique : Elle permet de désigner le COI de la phrase (vis à vis de quoi est faite l'action, à qui ?, pour qui ?, contre qui ?).",
+                "Particule syntaxique : Elle permet de désigner le changement.",
+                "Particule syntaxique : Elle permet de désigner la personne que l'on rencontre.",
+                "Particule syntaxique : Elle permet de désigner le complément de temps quand il s'agit d'un point ou d'une période précise (ex : le 6 août, à 6h, etc).",
+            ],
+            "で": [
+                "Particule syntaxique : Elle permet d'exprimer les lieux où il se passe quelque chose.",
+                "Particule syntaxique : Elle permet de préciser le moyen utilisé pour effectuer une action.",
+            ],
+            "へ": [
+                "Particule syntaxique : Elle permet d'indiquer la direction avec un verbe de mouvement directionnel.",
+            ],
+            "への": [
+                "Particule syntaxique : Elle permet d'indiquer la direction de quelque chose.",
+            ],
+            "を": [
+                "Particule syntaxique : Elle permet d'indiquer le COD de la phrase (qui subit l'action du verbe).",
+                "Particule syntaxique : Elle permet d'exprimer un lieu dans une idée de parcours (avec un verbe de mouvement).",
+            ],
+            "から": [
+                "Particule syntaxique : Elle permet d'exprimer le point de départ (avec une idée de “limite” / “jusqu'à”).",
+                "Particule conjonctive : Elle permet d'exprimer la cause entre 2 propositions dans une idée subjective (puisque, comme, car).",
+            ],
+            "まで": [
+                "Particule syntaxique : Elle permet d'exprimer le point d'arrivé (avec une idée de “limite” / “jusqu'à”).",
+            ],
+            "からの": [
+                "Particule syntaxique : Elle permet d'exprimer l'origine (d'un objet, d'un animal, etc).",
+            ],
+            "が": [
+                "Particule syntaxique : Elle permet d'exprimer le sujet du verbe.",
+                "Particule syntaxique : Elle permet d'exprimer le verbe avoir.",
+                "Particule conjonctive : Elle permet d'exprimer la concession entre 2 propositions sous une forme plus soutenue.",
+                "Particule conjonctive : Elle permet d'introduire une seconde proposition sans idée de concession.",
+            ],
+            "より": [
+                "Particule syntaxique : Elle permet de préciser le point de départ sous une forme plus soutenue.",
+                "Particule syntaxique : Elle permet d'indiquer la base pour une comparaison (par rapport à, comparé à, etc).",
+            ],
+            "だけ": [
+                "Particule adverbiale : Elle permet d'exprimer le caractère unique.",
+            ],
+            "のみ": [
+                "Particule adverbiale : Elle permet d'exprimer le caractère unique sous une forme plus soutenue.",
+            ],
+            "しか": [
+                "Particule adverbiale : Elle permet d'exprimer le caractère unique sous une forme négative.",
+            ],
+            "ばかり": [
+                "Particule adverbiale : Elle permet d'exprimer le caractère unique sous une forme péjorative.",
+            ],
+            "くらい": [
+                "Particule adverbiale : Elle permet d'exprimer une approximation dans le temps.",
+            ],
+            "ころ": [
+                "Particule adverbiale : Elle permet d'exprimer une approximation dans le temps lorsque le point est fixe dans le temps.",
+            ],
+            "ほど": [
+                "Particule adverbiale : Elle permet d'exprimer une approximation dans une idée de comparaison.",
+            ],
+            "など": [
+                "Particule adverbiale : Elle permet d'exprimer la non-exhaustivité et ainsi d'appuyer l'utilisation de や.",
+            ],
+            "なんか": [
+                "Particule adverbiale : Elle permet d'exprimer la non-exhaustivité et ainsi d'appuyer l'utilisation de や sous une forme familière.",
+            ],
+            "は": [
+                "Particule d'emphase : Elle permet d'exprimer le thème de la phrase.",
+            ],
+            "も": [
+                "Particule d'emphase : Elle permet d'introduire dans une phrase, une chose identique à une autre précédemment énoncée (aussi, de même, etc).",
+            ],
+            "こそ": [
+                "Particule d'emphase : Elle permet d'exprimer la détermination (pour sûr !) et vient remplacer は.",
+            ],
+            "さえ": [
+                "Particule d'emphase : Elle permet d'indiquer quelque chose d'étonnant dans une idée négative et vient remplacer は.",
+            ],
+            "でも": [
+                "Particule d'emphase : Elle permet d'indiquer quelque chose d'extrême dans une idée de moquerie et vient remplacer は.",
+                "Particule d'emphase : Elle permet de proposer autre chose lors d'un choix (ou autre).",
+            ],
+            "よ": [
+                "Particule de fin de phrases : Elle permet d'accentuer sa phrase. On peut le représenter par un “!” en français. Cela permet d'affirmer son avis.",
+            ],
+            "ね": [
+                "Particule de fin de phrases : Elle permet de confirmer (baisser l'intonation) ou demander une confirmation (monter l'intonation). C'est plutôt utilisé par les femmes.",
+            ],
+            "な": [
+                "Particule de fin de phrases : Elle permet de souligner son opinion à soi-même. La particule est plutôt utilisée par les hommes dans un sens familier.",
+            ],
+            "わ": [
+                "Particule de fin de phrases : Elle permet d'exprimer son entente, qu'on est favorable. C'est plutôt utilisé par les femmes.",
+            ],
+            "ぜ": [
+                "Particule de fin de phrases : Elle permet d'exprimer son entente, qu'on est favorable. C'est plutôt utilisé par les hommes et sous une forme familier.",
+                "Particule de fin de phrases : Elle permet d'accentuer sa phrase sous une forme vulgaire.",
+            ],
+            "とも": [
+                "Particule de fin de phrases : Elle permet d'exprimer une évidence.",
+            ],
+            "ぞ": [
+                "Particule de fin de phrases : Elle permet d'accentuer sa phrase de manière forte. Peut être utilisé pour donner un ordre ou indiquer un danger. C'est surtout utilisé par les hommes sous une forme familière.",
+            ],
+            "さ": [
+                "Particule de fin de phrases : Elle permet d'accentuer sa phrase tout en attirant l'attention de l'interlocuteur. C'est surtout utilisé par les hommes sous une forme familière.",
+            ],
+            "かしら": [
+                "Particule de fin de phrases : Elle permet d'indiquer une incertitude, de se questionner soi-même. C'est plus utilisé par les femmes.",
+            ],
+            "けれども": [
+                "Particule conjonctive : Elle permet d'exprimer la concession entre 2 propositions sous une douce.",
+            ],
+            "のに": [
+                "Particule conjonctive : Elle permet d'exprimer la concession entre 2 propositions dans un sens de surprise, regret ou mécontentement.",
+            ],
+            "ので": [
+                "Particule conjonctive : Elle permet d'exprimer la cause entre 2 propositions dans une idée objective (puisque, comme, car).",
+            ],
+            "のだ": [
+                "Tournure explicative : Placé en fin de phrase (juste après le verbe), elle permet de donner une explication.",
+            ],
+            "のか": [
+                "Tournure explicative : Placé en fin de phrase (juste après le verbe), elle permet de demander une explication.",
+            ],
         }
     };
 })();
